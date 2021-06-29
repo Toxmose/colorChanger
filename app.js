cf82a7fc3cf447a69ad31911ca93250f65ccc03c
toxmose <jeroen.vandroemme@gmail.com>
2021-06-29 22:44:13 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+let container = document.querySelector(".container");
+const span = document.querySelector("#span");
+
+// eventlistener button
+const clicker = document.querySelector("#btn");
+clicker.addEventListener("click", () => {
+  // generate 3 randon numbers for RGB
+  const color1 = Math.floor(Math.random() * 255) + 1;
+  const color2 = Math.floor(Math.random() * 255) + 1;
+  const color3 = Math.floor(Math.random() * 255) + 1;
+  console.log(color1, color2, color3);
+  // set backgroundcolor to random values
+  container.style.backgroundColor = `rgb(${color1},${color2},${color3})`;
+  span.innerHTML = `Red: ${color1} / Green: ${color2} / Blue: ${color3}`;
+  //   change text depending on color tone of background
+  if (color1 < 50 || color2 < 50 || color3 < 50) {
+    container.style.color = "#fff";
+  } else container.style.color = "#000";
+});
